a4a169c24456e50dcf38aac3f28d2449df52758a
hj1940 <35285265+hj1940@users.noreply.github.com>
2023-03-07 21:41:26 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -69,28 +69,3 @@ window.addEventListener("mouseup", () => {
     window.removeEventListener("mousemove", swipeDirection);
 });
 
-
-
-
-// 계좌번호 복사하기
-var copybtn = document.querySelector('.copybtn')
-var copyacc = document.querySelector('.acc').innerHTML
-const copy = (text) => {
-    // 임시의 textarea 생성
-    const $textarea = document.createElement("textarea");
-  
-    // body 요소에 존재해야 복사가 진행됨
-    document.body.appendChild($textarea);
-    
-    // 복사할 특정 텍스트를 임시의 textarea에 넣어주고 모두 셀렉션 상태
-    $textarea.value = text;
-    $textarea.select();
-    
-    // 복사 후 textarea 지우기
-    document.execCommand('copy');
-    document.body.removeChild($textarea);
-}
-
-copybtn.addEventListener('click', ()=>{
-    copy(copyacc);
-})
